a054518568dd4531300da56277216aec6ed5aab2
Lili <elizangelaapferreira14@gmail.com>
2026-02-11 20:28:48 -0300

    Creating main loop Connecting Menu and Game Classes

--- code/menu.py
+++ code/menu.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 class Menu:
-    def __init__(self):
-        self.windown = None
+    def __init__(self, windown):
+        self.windown = windown
 
     def run(self, ):
         pass
